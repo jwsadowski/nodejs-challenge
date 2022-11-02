94805d6108628e4657cb408f36f9ce07bfb17c35
Jon Sadowski <jwsadowski@gmail.com>
2022-11-02 18:01:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';  
+
+// TODO: Create an array of questions for user input
+const questions = [
+    type = 'input',
+    message = 'What is your username?',
+    name = 'question',
+
+    type = 'input',
+    message = 'What is your email address?',
+    name = 'question'
+];
+
+console.log(questions);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
